7a813161c80be0f7c17da64383fcf05b68cce5d8
Alireza Atashnejad <32798129+Mratashnejad@users.noreply.github.com>
2024-03-09 16:46:03 +0400

    conditionals

--- src/App.js
+++ src/App.js
@@ -2,19 +2,34 @@ import './App.css';
 
 //parent component
 function App() {
-//variable
-  return (
-    <div className="App">
-      <header className="App-header">
-        hello world
 
-        {/* chiled component*/}
-        <GetNameComponnent name = 'reza' age ={23} email="alireza@gmail.com"/>
-        <GetNameComponnent name = 'ali' age ={20} email="ali@gmail.com"/>
-        <Job salary = {205000} position = "react dev" company = "amazon" />
-      </header>
+  const age  = 20;
+  // if (age>20) console.log('over age')
+  // else console.log('under age')
+  //OR
+  //age > 20 ? console.log('over age') : console.log('under age')
+  //if (age>20)
+  //variable
+  return (
+    <div className='App'> 
+      {age > 20 ? <h1> Over 20 </h1>  : <h1>Under 20 </h1>}
+      {age === 20 && <h1> Age is  equal to 20</h1>}
+      
+      <button type='submit' className={age > 20 ? "success" : "warninng"}>click me</button>
     </div>
+
+  //   <div className="App">
+  //     <header className="App-header">
+  //       Over
+  //       {/* chiled component*/}
+  //       {/* <GetNameComponnent name = 'reza' age ={23} email="alireza@gmail.com"/>
+  //       <GetNameComponnent name = 'ali' age ={20} email="ali@gmail.com"/>
+  //       <Job salary = {205000} position = "react dev" company = "amazon" />\
+  // */}
+  //     </header>
+  //   </div>
   );
+  // return <div className='App'><header className="App-header"> Under</header></div>
 }
 
 //funcation
