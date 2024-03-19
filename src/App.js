9fb161552d104a66f504f813a54555cb8c087367
Alireza Atashnejad <32798129+Mratashnejad@users.noreply.github.com>
2024-03-19 07:57:35 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
 import './App.css';
-
+import {User} from "./User";
 //parent component
 function App() {
 
-  const age  = 20;
+  const age  = 2;
   // if (age>20) console.log('over age')
   // else console.log('under age')
   //OR
   //age > 20 ? console.log('over age') : console.log('under age')
   //if (age>20)
+  
+
+  //lists : filter , forech , map
+  const names = ["ali" , "reza" , "xashi" , "afi"]
+  
+  const users = [
+    {name: "ali" , age:31},
+    {name:"reza" , age:7},
+    {name:"xashi" , age:2},
+    {name:"afi" , age :30}
+  ]
   //variable
   return (
-    <div className='App'> 
-      {age > 20 ? <h1> Over 20 </h1>  : <h1>Under 20 </h1>}
-      {age === 20 && <h1> Age is  equal to 20</h1>}
-      
-      <button type='submit' className={age > 20 ? "success" : "warninng"}>click me</button>
+    <div>
+      {users.map((user,index)=>{
+        return <User key={index} name={user.name} age={user.age} />
+      })}
+
     </div>
+
+    // <div>
+    //   {users.map((user , index)=>{
+    //     return <h2 key ={index}> {user.name} : {user.age}</h2>
+    //   })}
+    // </div>
+
+
+    // <div>
+    //   {names.map((name , index)=>{
+    //       return <h1 key={index}>{name}</h1>
+    //   })}
+    // </div>
+
+
+
+    // <div className='App'> 
+      
+    //   {age > 20 ? <h1> Over 20 </h1>  : <h1>Under 20 </h1>}
+    //   {age === 20 && <h1> Age is  equal to 20</h1>}
+      
+    //   {/* <button type='submit' className={age > 20 ? "success" : "warninng"}>click me</button> */}
+    //   <button type='submit' style ={{fontSize : age > 20 ? "20px" : "30px"}}>click me</button>
+    // </div>
 
   //   <div className="App">
   //     <header className="App-header">
@@ -31,6 +66,7 @@ function App() {
   );
   // return <div className='App'><header className="App-header"> Under</header></div>
 }
+
 
 //funcation
 const getName = ()=>{
